51ad4838fd1315351c955cbde3c56dd40f2abd95
harshp <parasharharsh16@gmail.com>
2024-03-30 19:37:14 +0530

    updated activation_function file to plot only relu, tanh and leakyrelu on given datapoints

--- activation_function.py
+++ activation_function.py
@@ -14,10 +14,12 @@ def tanh(x):
     return np.tanh(x)
 
 def plot_function(x, plots):
+    file_sufix = ""
     for plot in plots:
         if plot == 'sigmoid':
             y = sigmoid(x)
             plt.plot(x, y, label='Sigmoid')
+            
         elif plot == 'relu':
             y = relu(x)
             plt.plot(x, y, label='ReLU')
@@ -27,15 +29,18 @@ def plot_function(x, plots):
         elif plot == 'tanh':
             y = tanh(x)
             plt.plot(x, y, label='Tanh')
-
+        file_sufix = file_sufix+'_'+plot
     plt.xlabel('x')
     plt.ylabel('Activation')
     plt.title('Activation function plot')
     plt.grid(True)
     plt.legend()
-    plt.savefig('activation_functions.png')
+    file_sufix = str(plots)
+    plt.savefig(f'activation_functions_{file_sufix}.png')
     plt.show()
 
 
-x = np.linspace(-10, 10, 100)
-plot_function(x, ['sigmoid', 'relu', 'leaky_relu', 'tanh'])
+x  = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
+#plot_function(x, ['sigmoid', 'relu', 'leaky_relu', 'tanh'])
+#ploting only relu, tanh and leaky_relu
+plot_function(np.array(x), ['relu', 'leaky_relu', 'tanh'])
